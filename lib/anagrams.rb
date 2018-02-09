b07c9fd364848bf05986876ff15ef99497b4ace7
Emily Watkins <emily.e.dolliver@gmail.com>
2018-02-09 10:21:33 -0800

    add rule to evaluate if there are any vowels, passes test.

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -8,10 +8,17 @@ class Anagram
     sort1 = @word1.downcase.chars.sort.join
     sort2 = @word2.downcase.chars.sort.join
 
+    count_vowels = sort2.chars.count {|l| l =~ /[aeiou]/}
+
+    if count_vowels == 0
+      return 'this isn\'t a word'
+    end
+
+
     if sort1 == sort2
-      "these words are anagrams"
+      'these words are anagrams'
     else
-      "these words aren't anagrams"
+      'these words aren\'t anagrams'
     end
 
   end
